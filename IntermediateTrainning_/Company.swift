6c77dd144715b06d0dbfe933e1b31d1d7b541c8b
xuau34 <btd84860@gmail.com>
2020-07-09 13:51:00 +0800

    patched

---
+++ IntermediateTrainning_/Company.swift
@@ -0,0 +1,15 @@
+//
+//  Company.swift
+//  IntermediateTrainning_
+//
+//  Created by 李宓2號 on 2020/7/2.
+//  Copyright © 2020 Mia. All rights reserved.
+//
+
+import Foundation
+
+struct Company {
+    let name: String
+    let founded: Date
+}
+
---
+++ IntermediateTrainning_/Company/CompaniesAutoController+FetchedResultController.swift
@@ -0,0 +1,9 @@
+//
+//  CompaniesAutoController+FetchedResultController.swift
+//  IntermediateTrainning_
+//
+//  Created by 李宓2號 on 2020/7/9.
+//  Copyright © 2020 Mia. All rights reserved.
+//
+
+import Foundation
